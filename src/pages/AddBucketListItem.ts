d27fb0067cfb61b64a2e321cf630ac482cfb0fc6
Tobias Riiser <tobias.riiser.games@gmail.com>
2025-07-17 11:01:36 +0200

    add bucket list items are not synced with LS themes

--- src/pages/AddBucketListItem.ts
+++ src/pages/AddBucketListItem.ts
@@ -1,4 +1,5 @@
 import { createBucketListItem } from '@/utils/bucketList.js'
+import { getThemes } from '@/utils/themes'
 import { getUsername } from '@/utils/user'
 
 // "Skapa" funktionalitet för bucketlist-arrayen
@@ -9,18 +10,27 @@ const formBucketlist: HTMLElement | null = document.querySelector('form')
 
 if (!(inputBucketlist instanceof HTMLInputElement)) {
     throw new Error('inputBucketlist is not intance of HTMLInputElement')
-} 
+}
 if (!(selectTheme instanceof HTMLSelectElement)) {
     throw new Error('selectTheme is not intance of HTMLSelectElement')
-} 
+}
 if (!(formBucketlist instanceof HTMLFormElement)) {
     throw new Error('formBucketlist is not intance of HTMLFormElement')
 }
 if (!(spanUsername instanceof HTMLSpanElement)) {
     throw new Error('spanUsername is not intance of HTMLSpanElement')
 }
-
 spanUsername.textContent = getUsername()
+
+// <option value="teknik">Teknikdrömmar</option>
+const themes: string[] = getThemes()
+themes.forEach((theme: string) => {
+    const optionElement = document.createElement('option')
+    optionElement.setAttribute('value', theme)
+    optionElement.innerText = theme
+    selectTheme.appendChild(optionElement)
+
+})
 
 formBucketlist.addEventListener('submit', (e) => {
     e.preventDefault()
